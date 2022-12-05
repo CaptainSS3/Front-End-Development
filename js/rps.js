e089913033cafcf43491b42d58df26720a436edd
Max <Blackruntr@gmail.com>
2022-12-05 19:55:14 +0200

    "finished real assignemnt conditions xD"

--- js/rps.js
+++ js/rps.js
@@ -1,7 +1,7 @@
 /* 
 Rock paper scissors 
-TODO 
 */
+let round = 0;
 let player_points = 0;
 let pc_points= 0;
 function computer_play(){
@@ -18,13 +18,16 @@ function computer_play(){
             return "scissors"
             break;
         default:
-          console.log("broken :(")
+            console.log("broken :(");
+
       } 
 }
 function player_selection(){
-    let hand = prompt("What would you like to play?");
-    result = hand.toLowerCase();
+    
+    let hand = prompt( `Round ${round} , What would you like to play ?`);
+    result = hand.toLowerCase().trim();
     return result;
+    
 }
 function play_round(){
     let hand_pc = computer_play();
@@ -32,40 +35,43 @@ function play_round(){
     if(hand_pc== "rock" && hand_player=="rock"){
         console.log("Computer has player rock you played rock, Its a TIE!");
     }
-    if(hand_pc== "rock" && hand_player=="paper"){
+    else if(hand_pc== "rock" && hand_player=="paper"){
         console.log("Computer has played rock you played paper, You win!");
         player_points++;
     }
-    if(hand_pc== "rock" && hand_player=="scissors"){
+    else if(hand_pc== "rock" && hand_player=="scissors"){
         console.log("Computer has played rock you played scissors, You lose!");
         pc_points++;
     }
-    if(hand_pc== "paper" && hand_player=="rock"){
+    else if(hand_pc== "paper" && hand_player=="rock"){
         console.log("Computer has played paper you played rock, You lose!");
         pc_points++;
     }
-    if(hand_pc== "paper" && hand_player=="paper"){ 
+    else if(hand_pc== "paper" && hand_player=="paper"){ 
         console.log("Computer has played paper you played paper, Its a TIE!");
     }
-    if(hand_pc== "paper" && hand_player=="scissors"){
+    else if(hand_pc== "paper" && hand_player=="scissors"){
         console.log("Computer has played paper you played scissors, You win!");
         player_points++;
     }
-    if(hand_pc== "scissors" && hand_player=="rock"){
+    else if(hand_pc== "scissors" && hand_player=="rock"){
         console.log("Computer has played scissors you played rock, You win!");
         player_points++;
     }
-    if(hand_pc== "scissors" && hand_player=="paper"){
+    else if(hand_pc== "scissors" && hand_player=="paper"){
         console.log("Computer has played scissors you played paper, You lose!");
         pc_points++;
     }
-    if(hand_pc== "scissors" && hand_player=="scissors"){
+    else if(hand_pc== "scissors" && hand_player=="scissors"){
         console.log("Computer has played scissors you played scissors, Its a TIE!");
+    }
+    else{
+        console.log("No hand was played, round will be skipped!")
     }
 }
 function game(){
-
     for (let i = 0; i < 5; i++) {
+        round++;
         play_round();
     }
     console.log(`The computer has ${pc_points} points, and you have ${player_points} points`)
@@ -78,6 +84,6 @@ function game(){
     if(pc_points == player_points){
         console.log("You tied with the computer!, maybe AI is not going to run the world after all!");
     }
-
 }
+prompt("Would you like to face the computer in a Rock paper scissors game?", "Yes iam a god at RPS");
 game();
