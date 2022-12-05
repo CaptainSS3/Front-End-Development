1c248f87e9ca4159a7e38d46cda621635a459f98
Max <Blackruntr@gmail.com>
2022-12-05 15:49:55 +0200

    "added a point system"

--- js/rps.js
+++ js/rps.js
@@ -1,6 +1,9 @@
 /* 
 Rock paper scissors 
+TODO 
 */
+let player_points = 0;
+let pc_points= 0;
 function computer_play(){
     let x = Math.floor(Math.random() * 3);
     switch(x) {
@@ -31,32 +34,50 @@ function play_round(){
     }
     if(hand_pc== "rock" && hand_player=="paper"){
         console.log("Computer has played rock you played paper, You win!");
+        player_points++;
     }
     if(hand_pc== "rock" && hand_player=="scissors"){
         console.log("Computer has played rock you played scissors, You lose!");
+        pc_points++;
     }
     if(hand_pc== "paper" && hand_player=="rock"){
         console.log("Computer has played paper you played rock, You lose!");
+        pc_points++;
     }
-    if(hand_pc== "paper" && hand_player=="paper"){
+    if(hand_pc== "paper" && hand_player=="paper"){ 
         console.log("Computer has played paper you played paper, Its a TIE!");
     }
     if(hand_pc== "paper" && hand_player=="scissors"){
         console.log("Computer has played paper you played scissors, You win!");
+        player_points++;
     }
     if(hand_pc== "scissors" && hand_player=="rock"){
         console.log("Computer has played scissors you played rock, You win!");
+        player_points++;
     }
     if(hand_pc== "scissors" && hand_player=="paper"){
         console.log("Computer has played scissors you played paper, You lose!");
+        pc_points++;
     }
     if(hand_pc== "scissors" && hand_player=="scissors"){
         console.log("Computer has played scissors you played scissors, Its a TIE!");
     }
 }
 function game(){
+
     for (let i = 0; i < 5; i++) {
         play_round();
     }
+    console.log(`The computer has ${pc_points} points, and you have ${player_points} points`)
+    if(pc_points < player_points){
+        console.log("Congradulations!, you have won");
+    }
+    if(pc_points > player_points){
+        console.log("Computer beat you, domestic abuse has accured, fbm");
+    }
+    if(pc_points == player_points){
+        console.log("You tied with the computer!, maybe AI is not going to run the world after all!");
+    }
+
 }
 game();
